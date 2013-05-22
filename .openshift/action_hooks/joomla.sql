371dab78c8bb1ab9fc2e78c5e4c5b91101c8d9eb
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2013-05-22 18:11:08 +0200

    Do not create the `joomla` database.

--- .openshift/action_hooks/joomla.sql
+++ .openshift/action_hooks/joomla.sql
@@ -19,7 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `joomla`
 --
-CREATE DATABASE `joomla` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `joomla`;
 
 -- --------------------------------------------------------
