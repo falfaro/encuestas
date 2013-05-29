b94b26e277c251f86d5517b0083fba36b4ab2ea1
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2013-05-29 13:28:45 +0200

    Add an admin model Votos that uses JModelList.

---
+++ php/com_encuestas/admin/models/votos.php
@@ -0,0 +1,29 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+jimport('joomla.application.component.modellist');
+
+/*
+ * Modelo Votos
+ */
+class EncuestasModelVotos extends JModelList
+{
+  /*
+   * Los metodos getItems() y getPagination() estan definidos en la clase
+   * JModelList y no hace falta redefinirlos aqui.
+   */
+
+  /*
+   * Metodo para construir una sentencia SQL para cargar los datos
+   * de la lista.
+   *
+   * @return      string  Una consulta SQL
+   */
+  protected function getListQuery()
+  {
+    $db = JFactory::getDBO();
+    $query = $db->getQuery(true);
+    $query->select('id,id_encuesta,id_elemento_encuesta,id_sesion,fecha');
+    $query->from('#__votos');
+    return $query;
+  }
+}
