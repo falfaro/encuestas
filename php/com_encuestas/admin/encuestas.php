45f5cc4a072e55871e7c99db46f078ededb52922
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2013-06-06 14:05:41 +0200

    Add basic support for ACLs to the encuestas view.

--- php/com_encuestas/admin/encuestas.php
+++ php/com_encuestas/admin/encuestas.php
@@ -1,12 +1,16 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
-jimport('joomla.application.component.controller');
 
 // Comprueba si el usuario tiene el nivel de acceso necesario para acceder
 // a la parte administrativa (backend) de este componente.
 if (!JFactory::getUser()->authorise('core.manage', 'com_encuestas'))  {
   return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
 }
+
+// Incluye la clase de ayuda
+JLoader::register('EncuestasHelper', dirname(__FILE__) . '/helpers/encuestas.php');
+ 
+jimport('joomla.application.component.controller');
 
 // Obtiene una instancia del controlador cuyo nombre esta prefijado por el
 // literal Encuestas.
--- php/com_encuestas/admin/views/encuestas/view.html.php
+++ php/com_encuestas/admin/views/encuestas/view.html.php
@@ -1,25 +1,57 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
  
-jimport( 'joomla.application.component.view' );
-jimport( 'joomla.html.pagination' );
+jimport('joomla.application.component.view');
 
 
-class EncuestasViewEncuestas extends JView
+class EncuestasViewEncuestas extends JViewLegacy
 {
+  // Atributos utilizados por la vista y las plantillas relacionadas.
+  protected $canDo;
+  protected $items;
+  protected $pagination;
+  protected $state;
 
-  function display($tpl = null)
-  {
-    // Toolbar
-    JToolBarHelper::title('Encuestas', 'generic.png' );
-    JToolBarHelper::addNewX('encuesta.add');
-    JToolBarHelper::editListX('encuesta.edit');
-    JToolBarHelper::deleteList('Confirmar borrado', 'encuestas.delete' );
+  function display($tpl = null) {
+    //    // Toolbar
+    //    JToolBarHelper::title('Encuestas', 'generic.png' );
+    //    JToolBarHelper::addNewX('encuesta.add');
+    //    JToolBarHelper::editListX('encuesta.edit');
+    //    JToolBarHelper::deleteList('Confirmar borrado', 'encuestas.delete' );
 
+    // Almacena el nivel de acceso que el usuario actual tiene sobre este
+    // componente.
+    $this->canDo = EncuestasHelper::getActions();
+
+    // Almacena los elementos necesarios para mostrar la lista de encuestas,
+    // asi como permitir la paginacion.
     $this->pagination = $this->get('Pagination');
     $this->items = $this->get('Items');
     $this->state = $this->get('State');
 
+    // Configura la barra de herramientas
+    $this->addToolBar();
+
     parent::display($tpl);
+  }
+
+  /*
+   * Configura la barra de herramientas
+   */
+  protected function addToolBar() {
+    JToolBarHelper::title('Administracion de Encuestas', 'encuestas');
+    if ($this->canDo->get('core.create')) {
+      JToolBarHelper::addNew('encuestas.add', 'JTOOLBAR_NEW');
+    }
+    if ($this->canDo->get('core.edit')) {
+      JToolBarHelper::editList('encuestas.edit', 'JTOOLBAR_EDIT');
+    }
+    if ($this->canDo->get('core.delete')) {
+      JToolBarHelper::deleteList('', 'encuestas.delete', 'JTOOLBAR_DELETE');
+    }
+    if ($this->canDo->get('core.admin')) {
+      JToolBarHelper::divider();
+      JToolBarHelper::preferences('com_encuestas');
+    }
   }
 }
