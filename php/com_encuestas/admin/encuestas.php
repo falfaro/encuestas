2e8f80e0bc027f6e7551c6458c0cc0ba72e48ffa
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2013-06-06 06:26:13 +0200

    Only allow access to backend for users that have the core.manage permission.

--- php/com_encuestas/admin/encuestas.php
+++ php/com_encuestas/admin/encuestas.php
@@ -1,7 +1,13 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 jimport('joomla.application.component.controller');
- 
+
+// Comprueba si el usuario tiene el nivel de acceso necesario para acceder
+// a la parte administrativa (backend) de este componente.
+if (!JFactory::getUser()->authorise('core.manage', 'com_encuestas'))  {
+  return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+}
+
 // Obtiene una instancia del controlador cuyo nombre esta prefijado por el
 // literal Encuestas.
 $controller =& JController::getInstance('Encuestas');
